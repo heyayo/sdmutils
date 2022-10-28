1ca036c7aaba7b22aadd5f266ebc898581fa3b96
heyayo <xalpe@pm.me>
2022-10-28 17:14:19 +0800

    delete from last commit

--- utility/english.hpp
+++
@@ -1,6 +0,0 @@
-namespace sdm
-{
-	// TODO
-	static constexpr char* numbers[]
-	{ "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty" };
-}
