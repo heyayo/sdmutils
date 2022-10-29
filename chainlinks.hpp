e7c1efbdd0bb34d13d499e9d6dc2e167322c0506
heyayo <xalpe@pm.me>
2022-10-29 15:17:15 +0800

    Linked List detachment and deconstructor

--- chainlinks.hpp
+++ chainlinks.hpp
@@ -1,6 +1,16 @@
 #ifndef SDMUTILS_CHAINLINKS_HPP
 #define SDMUTILS_CHAINLINKS_HPP
 
+/*
+*  SDMUTILS
+*  An Attempt to Recreate Things from STL and more
+* 
+*  CHAINLINKS Includes
+*  LinkedList
+*  LinkedNode
+*  LinkedListIterator W.I.P unsafe
+*/
+
 #include <iostream>
 
 namespace sdm
@@ -22,7 +32,7 @@ namespace sdm
 
         constexpr void Link(LPtr oLink)
         { m_Link = oLink; }
-        inline LRef Traverse(int index)
+        inline LRef Traverse(int index) // unsafe
         {
             LPtr temp = m_Link;
             for ( int i = 1; i < index; ++i )
@@ -57,7 +67,7 @@ namespace sdm
     };
 
 	template<class LinkedNode>
-	class LinkedListIterator
+	class LinkedListIterator // W.I.P
 	{
 		public:
 		using Type = LinkedNode;
@@ -109,24 +119,43 @@ namespace sdm
 			m_TAIL = m_HEAD;
 			m_Capacity = 1;
 		}
+		LinkedList()
+		{
+			m_HEAD = nullptr;
+			m_TAIL = nullptr;
+			m_Capacity = 0;
+		}
 		// TODO DECONSTRUCTOR
+		~LinkedList()
+		{
+			auto next = m_HEAD;
+			auto curr = m_HEAD;
+			for (int i = 0; i < m_Capacity; ++i)
+			{
+				next = curr->Next();
+				delete curr;
+				curr = next;
+			}
+		}
 
 		private:
 		NodePtr m_HEAD;
 		NodePtr m_TAIL;
-		unsigned m_Capacity;
+		int m_Capacity;
 
 		public:
 		constexpr NodePtr first()
 		{ return m_HEAD; }
 		constexpr NodePtr last()
 		{ return m_TAIL; }
-		constexpr unsigned capacity()
+		constexpr int capacity()
 		{ return m_Capacity;}
 		constexpr Iter begin()
 		{ return Iter(m_HEAD); }
 		constexpr Iter end()
 		{ return Iter(m_TAIL); }
+		constexpr bool isEmpty()
+		{ return m_Capacity <= 0; }
 
 		constexpr NodeRef operator[](const int index)
 		{
@@ -136,28 +165,74 @@ namespace sdm
 			return *temp;
 		}
 
-		constexpr void chainback(Type data)
+		constexpr void chainback(Type data) // Add node from Tail End
 		{
-			NodePtr temp = new Node(data);
-			m_TAIL->Link(temp);
-			m_TAIL = temp;
-			++m_Capacity;
+			NodePtr temp = new Node(data); // new node
+			if (isEmpty())
+			{
+				m_HEAD = temp; // Initialize HEAD
+				m_TAIL = temp; // Initialize TAIL
+			}
+			else
+			{
+				m_TAIL->Link(temp); // Link new node as new tail
+				m_TAIL = temp; // Reset tail to new node
+			}
+			++m_Capacity; // Increase capacity
 		}
-		constexpr void chainfront(Type data)
+		constexpr void chainfront(Type data) // Add node from Head End
 		{
-			NodePtr temp = new Node(data);
-			temp->Link(m_HEAD);
-			m_HEAD = temp;
-			++m_Capacity;
+			NodePtr temp = new Node(data); // new node
+			if (isEmpty())
+			{
+				m_HEAD = temp; // Initialize HEAD
+				m_TAIL = temp; // Initialize TAIL
+			}
+			else
+			{
+				temp->Link(m_HEAD); // Link new node as new head
+				m_HEAD = temp; // Reset head to new node
+			}
+			++m_Capacity; // Increase capacity
+		}
+		constexpr Type detach(int index) // Detach Node and return
+		{
+			--m_Capacity; // Decrease capacity
+			if (index == 0)
+			{
+				auto temp = m_HEAD;
+				m_HEAD = m_HEAD->Next();
+				Type returnval = temp->Value();
+				delete temp;
+				return returnval;
+			}
+			auto prev = m_HEAD;; // The node behind
+			for (int i = 1; i < index; ++i)
+			{
+				prev = prev->Next(); // Going to the node behind
+			}
+			auto curr = prev->Next(); // Going to the node to delete
+			if (curr->IsEnd()) // If deleting last node
+			{
+				m_TAIL = prev; // Set tail to behind
+				prev->Link(nullptr); // Make node point to nullptr
+			}
+			else // If not deleting last node
+				prev->Link(curr->Next()); // Detach node from links and relink behind and after
+			Type returnval = curr->Value(); // Save return value
+			delete curr; // Delete node
+			return returnval;
 		}
 
 		constexpr friend std::ostream& operator<<(std::ostream& stream, LinkedList& other)
 		{
-			for (int i = 0; i < other.capacity(); ++i)
+			auto curr = other.first();
+			while (!curr->IsEnd())
 			{
-				stream << other[i] << ' ';
+				stream << curr->Value() << ' ';
+				curr = curr->Next();
 			}
-
+			stream << curr->Value() << ' ';
 			return stream;
 		}
 	};
