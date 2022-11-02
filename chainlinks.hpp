9e9ae04dd502f2e213fdf7cc8cccba9c33dd2d26
heyayo <xalpe@pm.me>
2022-11-02 16:01:54 +0800

    Double Linked List, Fixed old issues

--- chainlinks.hpp
+++ chainlinks.hpp
@@ -16,55 +16,131 @@
 namespace sdm
 {
     template<typename Type>
-    class LinkedNode
+    class BaseNode
     {
 		public:
-        using L = LinkedNode;
-        using LRef = LinkedNode&;
-        using LPtr = LinkedNode*;
+        using N = BaseNode;
+        using NRef = N&;
+        using NPtr = N*;
 
-		private:
-        LinkedNode* m_Link;
+		protected:
         Type data;
+		BaseNode(Type payload) : data(payload) { std::cout << "DEBUG BASENODE" << std::endl; }
+		~BaseNode() { std::cout << "DEBUG BASENODE DEATH" << std::endl; }
 
         public:
-        LinkedNode(Type payload) : data(payload), m_Link(nullptr) {}
+		constexpr Type& value() { return data; }
 
-        constexpr void Link(LPtr oLink)
-        { m_Link = oLink; }
-        inline LRef Traverse(int index) // unsafe
-        {
-            LPtr temp = m_Link;
-            for ( int i = 1; i < index; ++i )
-            {
-                temp = temp->GetLink();
-            }
-            return *temp;
-        }
-        constexpr bool IsEnd() { return m_Link == nullptr; }
-		constexpr Type& Value() { return data; }
-		constexpr LPtr Next() { return m_Link; }
-        
         // Operator Overloads
-        constexpr LRef operator++() { ++data; return *this; }
-        constexpr LRef operator++(int) { data++; return *this; }
-        constexpr LRef operator--() { ++data; return *this; }
-        constexpr LRef operator--(int) { data++; return *this; }
-        constexpr LRef operator+=(const LRef other) { data += other.data; return *this; }
-        constexpr LRef operator-=(const LRef other) { data -= other.data; return *this; }
-        constexpr L operator+(const LRef other) { L temp = *this; return temp += other; }
-        constexpr L operator-(const LRef other) { L temp = *this; return temp -= other; }
-        constexpr LRef operator*() { return data; }
-        constexpr LPtr operator->() { return &data; }
-        constexpr bool operator==(const LRef other) { return data == other.data; }
-        constexpr bool operator!=(const LRef other) { return data != other.data; }
-        constexpr bool operator<(const LRef other) { return data < other.data; }
-        constexpr bool operator>(const LRef other) { return data > other.data; }
-        constexpr bool operator>=(const LRef other) { return data >= other.data; }
-        constexpr bool operator<=(const LRef other) { return data <= other.data; }
-        constexpr friend std::ostream& operator<<(std::ostream& stream, const LRef other)
-        { stream << other.data; return stream; }
+        constexpr NRef operator++() { ++data; return *this; }
+        constexpr N operator++(int) { N temp = *this; ++data; return temp; }
+        constexpr NRef operator--() { --data; return *this; }
+        constexpr N operator--(int) { N temp = *this; --data; return temp; }
+        constexpr NRef operator+=(const NRef other) { data += other.data; return *this; }
+        constexpr NRef operator-=(const NRef other) { data -= other.data; return *this; }
+        constexpr N operator+(const NRef other) { N temp = *this; return temp += other; }
+        constexpr N operator-(const NRef other) { N temp = *this; return temp -= other; }
+        constexpr NRef operator*() { return *this; }
+        constexpr NPtr operator->() { return this; }
+        constexpr bool operator==(const NRef other) { return data == other.data; }
+        constexpr bool operator!=(const NRef other) { return data != other.data; }
+        constexpr bool operator<(const NRef other) { return data < other.data; }
+        constexpr bool operator>(const NRef other) { return data > other.data; }
+        constexpr bool operator>=(const NRef other) { return data >= other.data; }
+        constexpr bool operator<=(const NRef other) { return data <= other.data; }
+        //constexpr friend std::ostream& operator<<(std::ostream& stream, const LRef other)
+        //{ stream << other.data; return stream; }
     };
+
+	/*
+	template<typename Type>
+	class FNode : virtual public BaseNode<Type>
+	{
+		public:
+		using N = FNode<Type>;
+		using NRef = N&;
+		using NPtr = N*;
+
+		protected:
+		NPtr m_Forward;
+
+		public:
+        FNode(Type payload) : BaseNode<Type>(payload), m_Forward(nullptr) { std::cout << "DEBUG FNODE" << std::endl; }
+
+        constexpr void linkFront(NPtr oLink)
+        { m_Forward = oLink; }
+        constexpr bool isHead() { return m_Forward == nullptr; }
+		constexpr NPtr prev() { return m_Forward; }
+	};
+	Same as BNode except worse
+	*/ 
+
+	template<typename Type>
+	class BNode : public BaseNode<Type>
+	{
+		public:
+		using N = BNode<Type>;
+		using NRef = N&;
+		using NPtr = N*;
+
+		protected:
+		NPtr m_Backward;
+
+		public:
+		BNode(Type payload) : BaseNode<Type>(payload), m_Backward(nullptr) { std::cout << "DEBUG BNODE" << std::endl; }
+		~BNode() { std::cout << "DEBUG BNODE DEATH" << std::endl; }
+
+		constexpr void linkBack(NPtr oLink)
+		{ m_Backward = oLink; }
+		constexpr bool isTail() { return m_Backward == nullptr; }
+		constexpr NPtr next() { return m_Backward; }
+	};
+
+	template<typename Type>
+	class DNode : public BaseNode<Type>
+	{
+		public:
+		using N = DNode<Type>;
+		using NRef = N&;
+		using NPtr = N*;
+
+		private:
+		NPtr m_Forward;
+		NPtr m_Backward;
+
+		public:
+		DNode(Type payload) : BaseNode<Type>(payload), m_Forward(nullptr), m_Backward(nullptr)
+		{ std::cout << "DEBUG DNODE" << std::endl; }
+		DNode() { std::cout << "DEBUG DNODE DEATH" << std::endl; }
+
+		constexpr void linkBack(NPtr oLink) { m_Backward = oLink; }
+		constexpr void linkFront(NPtr oLink) { m_Forward = oLink; }
+		constexpr bool isHead() { return m_Forward == nullptr; }
+		constexpr bool isTail() { return m_Backward == nullptr; }
+		constexpr NPtr next() { return m_Backward; }
+		constexpr NPtr prev() { return m_Forward; }
+	};
+
+	/* Depreciated In Favor of DNode
+	template<typename Type>
+	class DualLinkedNode : public LinkedNode<Type>
+	{
+		using L = DualLinkedNode;
+		using LRef = L&;
+		using LPtr = L*;
+
+		LPtr m_Backward;
+
+		public:
+		DualLinkedNode(Type payload) : LinkedNode<Type>(payload), m_Backward(nullptr) {}
+
+		constexpr void LinkBack(LPtr oLink)
+		{ m_Backward = oLink; }
+		constexpr LPtr Back() { return m_Backward; }
+		constexpr bool IsHead() { return m_Backward == nullptr; }
+
+	};
+	*/
 
 	template<class LinkedNode>
 	class LinkedListIterator // W.I.P
@@ -100,16 +176,14 @@ namespace sdm
 		{ return m_Ptr != other.m_Ptr; }
 	};
 
-	template<typename Type>
+	template<template<typename> class NodeType, typename Type>
 	class LinkedList
 	{
 		public:
-		using TypeRef = Type&;
-		using TypePtr = Type*;
-		using Node = LinkedNode<Type>;
+		using Node = NodeType<Type>;
 		using NodeRef = Node&;
 		using NodePtr = Node*;
-		using Iter = LinkedListIterator<LinkedNode<Type>>;
+		using Iter = LinkedListIterator<Node>;
 		using IterRef = Iter&;
 		using IterPtr = Iter*;
 
@@ -125,17 +199,19 @@ namespace sdm
 			m_TAIL = nullptr;
 			m_Capacity = 0;
 		}
-		// TODO DECONSTRUCTOR
 		~LinkedList()
 		{
 			auto next = m_HEAD;
 			auto curr = m_HEAD;
-			for (int i = 0; i < m_Capacity; ++i)
+			m_HEAD = nullptr;
+			m_TAIL = nullptr;
+			for (int i = 1; i < m_Capacity; ++i)
 			{
-				next = curr->Next();
+				next = curr->next();
 				delete curr;
 				curr = next;
 			}
+			delete curr;
 		}
 
 		private:
@@ -161,7 +237,7 @@ namespace sdm
 		{
 			auto temp = m_HEAD;
 			for ( int i = 0; i < index; ++i )
-			{ temp = temp->Next(); }
+			{ temp = temp->next(); }
 			return *temp;
 		}
 
@@ -175,8 +251,19 @@ namespace sdm
 			}
 			else
 			{
-				m_TAIL->Link(temp); // Link new node as new tail
-				m_TAIL = temp; // Reset tail to new node
+				if constexpr (std::is_same<BNode<Type>,Node>::value)
+				{
+					m_TAIL->linkBack(temp);
+					temp->linkBack(nullptr);
+					m_TAIL = temp;
+				}
+				if constexpr (std::is_same<DNode<Type>,Node>::value)
+				{
+					m_TAIL->linkBack(temp);
+					temp->linkFront(m_TAIL);
+					temp->linkBack(nullptr);
+					m_TAIL = temp;
+				}
 			}
 			++m_Capacity; // Increase capacity
 		}
@@ -190,8 +277,18 @@ namespace sdm
 			}
 			else
 			{
-				temp->Link(m_HEAD); // Link new node as new head
-				m_HEAD = temp; // Reset head to new node
+				if constexpr (std::is_same<BNode<Type>,Node>::value)
+				{
+					temp->linkBack(m_HEAD);
+					m_HEAD = temp;
+				}
+				if constexpr (std::is_same<DNode<Type>,Node>::value)
+				{
+					m_HEAD->linkFront(temp);
+					temp->linkBack(m_HEAD);
+					temp->linkFront(nullptr);
+					m_HEAD = temp;
+				}
 			}
 			++m_Capacity; // Increase capacity
 		}
@@ -200,42 +297,91 @@ namespace sdm
 			--m_Capacity; // Decrease capacity
 			if (index == 0)
 			{
-				auto temp = m_HEAD;
-				m_HEAD = m_HEAD->Next();
-				Type returnval = temp->Value();
-				delete temp;
+				if constexpr (std::is_same<BNode<Type>,Node>::value || std::is_same<DNode<Type>,Node>::value)
+				{
+					auto temp = m_HEAD;
+					m_HEAD = m_HEAD->next();
+					if constexpr (std::is_same<DNode<Type>,Node>::value)
+					{
+						m_HEAD->linkFront(nullptr);
+					}
+					Type returnval = temp->value();
+					delete temp;
+					return returnval;
+				}
+			}
+			if constexpr (std::is_same<BNode<Type>,Node>::value || std::is_same<DNode<Type>,Node>::value)
+			{
+				auto prev = m_HEAD; // The node behind
+				for (int i = 1; i < index; ++i)
+				{
+					prev = prev->next(); // Going to the node behind
+				}
+				auto curr = prev->next(); // Going to the node to delete
+				if (curr->isTail()) // If deleting last node
+				{ 
+					m_TAIL = prev; // Set tail to behind
+					prev->linkBack(nullptr); // Make node point to nullptr
+				} 
+				else // If not deleting last node
+				{
+					prev->linkBack(curr->next()); // Detach node from links and relink behind and after
+					if constexpr (std::is_same<DNode<Type>,Node>::value)
+					{
+						curr->next()->linkFront(prev);
+					}
+				}
+				Type returnval = curr->value(); // Save return value
+				delete curr; // Delete node
 				return returnval;
 			}
-			auto prev = m_HEAD;; // The node behind
-			for (int i = 1; i < index; ++i)
-			{
-				prev = prev->Next(); // Going to the node behind
-			}
-			auto curr = prev->Next(); // Going to the node to delete
-			if (curr->IsEnd()) // If deleting last node
-			{
-				m_TAIL = prev; // Set tail to behind
-				prev->Link(nullptr); // Make node point to nullptr
-			}
-			else // If not deleting last node
-				prev->Link(curr->Next()); // Detach node from links and relink behind and after
-			Type returnval = curr->Value(); // Save return value
-			delete curr; // Delete node
-			return returnval;
 		}
 
 		constexpr friend std::ostream& operator<<(std::ostream& stream, LinkedList& other)
 		{
 			auto curr = other.first();
-			while (!curr->IsEnd())
+			while (!curr->isTail())
 			{
-				stream << curr->Value() << ' ';
-				curr = curr->Next();
+				stream << curr->value() << ',';
+				curr = curr->next();
 			}
-			stream << curr->Value() << ' ';
+			stream << curr->value();
 			return stream;
 		}
+		/*
+		constexpr friend std::ostream& operator<<(std::ostream& stream, NodeRef other)
+		{
+			auto curr = other.first();
+			while (!curr->isTail())
+			{
+				stream << curr->value() << ' ';
+				curr = curr->next();
+			}
+			stream << curr->value() << ' ';
+			return stream;
+		}
+		*/
 	};
+
+	/* Unused In Favor of more templates
+	template<typename Type>
+	class DualLinkedList : public LinkedList<Type>
+	{
+		public:
+		using Node = DualLinkedNode<Type>;
+		using NodeRef = Node&;
+		using NodePtr = Node*;
+
+		private:
+		NodePtr m_HEAD;
+		NodePtr m_TAIL;
+		int m_Capacity;
+
+		public:
+		DualLinkedList(Type first) : LinkedList<Type>(first) {}
+		
+	};
+	*/
 }
 
 #endif
